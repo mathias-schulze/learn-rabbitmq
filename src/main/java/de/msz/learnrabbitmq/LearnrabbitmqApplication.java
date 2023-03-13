b66ca1ecb75fbaa8cd8c5344683df8b7e83e7e1a
Mathias Schulze <mathias-schulze@web.de>
2023-03-13 22:21:29 +0100

    Spring Boot project generated with Spring Initializr

---
+++ src/main/java/de/msz/learnrabbitmq/LearnrabbitmqApplication.java
@@ -0,0 +1,13 @@
+package de.msz.learnrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnrabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnrabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/msz/learnrabbitmq/LearnrabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package de.msz.learnrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnrabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
