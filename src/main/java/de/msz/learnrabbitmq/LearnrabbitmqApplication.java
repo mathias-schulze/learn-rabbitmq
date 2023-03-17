0b8bfa1f05c35b37b44c248fe22efbd984b82ce1
Mathias Schulze <mathias-schulze@web.de>
2023-03-18 00:01:18 +0100

    send first message with RabbitMQ

--- src/main/java/de/msz/learnrabbitmq/LearnrabbitmqApplication.java
+++ src/main/java/de/msz/learnrabbitmq/LearnrabbitmqApplication.java
@@ -1,13 +1,23 @@
 package de.msz.learnrabbitmq;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnrabbitmqApplication {
+public class LearnrabbitmqApplication implements CommandLineRunner {
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnrabbitmqApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		rabbitTemplate.convertAndSend("TestExchange", "TestRouting", "Hello RabbitMQ!");
+	}
 }
