7000a3a87df27080a711fea04f12fe2fd2442ff1
Mathias Schulze <mathias-schulze@web.de>
2023-03-18 21:50:50 +0100

    receiver project generated by Spring Initializr

---
+++ learn-rabbitmq-receiver/src/main/java/de/msz/learnrabbitmqreceiver/LearnRabbitmqReceiverApplication.java
@@ -0,0 +1,13 @@
+package de.msz.learnrabbitmqreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRabbitmqReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnRabbitmqReceiverApplication.class, args);
+    }
+
+}
